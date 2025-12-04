11c4827c393a89baefede7403f198236ba72fdb5
Vincent Goh <vincent.goh@vincentgoh.local>
2025-12-04 16:42:16 +0800

    test3333

---
+++ test2.py
@@ -0,0 +1,50 @@
+
+
+
+# ing
+v = 1
+
+# string
+v2 = 'string'
+
+# list 
+v3 = [1,2,3]
+
+# dictionary
+v4 = {
+    'name': 'John',
+    'age': 30,
+    'city': 'New York'
+}
+
+# tuple
+v5 = (1,2,3)
+
+
+
+
+# print (v3[2])
+
+# print (v4['name'])
+
+# print (v5[2])
+
+
+
+
+v4['tel'] = '12324234343'
+
+
+print(v4)
+
+
+
+v3.append(4)
+v3.append(5)
+for i in v3:
+    print("v3 item:", i)
+
+
+
+
+print(v+5)
